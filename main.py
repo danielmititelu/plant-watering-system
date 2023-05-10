e3761f27bfe70d0b66bf648947fa2a68db9150f6
Dumitru-Daniel Mititelu <mttl.daniel@gmail.com>
2023-05-10 21:19:17 +0300

    Refactor code

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ MINUTE = 60
 HOUR = 60 * MINUTE
 DAY = 24 * HOUR
 UTC_OFFSET = 3
-WATERING_TIME = 12
+PUMP_START_AT_HOUR = 9
+PUMP_START_INTERVAL_DAYS = 1
+PUMP_OPENED_DURATION_SECONDS = 10
 
 
 def connect_to_wlan():
@@ -21,11 +23,7 @@ def connect_to_wlan():
     wlan.active(True)
     wlan.connect(ssid, password)
     while wlan.isconnected() == False:
-        print('Waiting for connection...')
         sleep(1)
-    ip = wlan.ifconfig()[0]
-    print(f'Connected on {ip}')
-
 
 def disconnect_from_wlan():
     wlan = network.WLAN(network.STA_IF)
@@ -34,60 +32,58 @@ def disconnect_from_wlan():
 
 
 def get_time_to_sleep(current_timestamp):
-    DAYS_TO_ADD = 0
-    if is_even_year_day(current_timestamp) and is_before_watering_time(current_timestamp):
-        DAYS_TO_ADD = 0
-    elif is_even_year_day(current_timestamp):
-        DAYS_TO_ADD = 2
-    else:
-        DAYS_TO_ADD = 1
-
-    target_timestamp = current_timestamp + DAYS_TO_ADD * DAY
-    target_timestamp = (target_timestamp // DAY) * DAY + WATERING_TIME * HOUR
+    # sleep till next day at PUMP_START_AT_HOUR
+    target_timestamp = current_timestamp + 1 * DAY
+    target_timestamp = (target_timestamp // DAY) * DAY + PUMP_START_AT_HOUR * HOUR
     return target_timestamp - current_timestamp 
 
 
 def open_pump():
     PUMP_PIN.value(1)
-    sleep(10)
+    sleep(PUMP_OPENED_DURATION_SECONDS)
     PUMP_PIN.value(0)
 
 
-def is_even_year_day(timestamp):
-    time_tuple = time.localtime(timestamp)
-    year_day = time_tuple[7]
-    return (year_day % 2) == 0
+def is_watering_day(timestamp):
+    return (get_day_of_year(timestamp) % PUMP_START_INTERVAL_DAYS) == 0
 
 
 def is_before_watering_time(timestamp):
-    time_tuple = time.localtime(timestamp)
-    hour = time_tuple[3]
-    return hour < WATERING_TIME
+    return get_hour(timestamp) < PUMP_START_AT_HOUR
 
 
 def is_watering_time(timestamp):
+    return get_hour(timestamp) == PUMP_START_AT_HOUR
+
+
+def get_hour(timestamp):
     time_tuple = time.localtime(timestamp)
-    hour = time_tuple[3]
-    return hour == WATERING_TIME
+    return time_tuple[3]
+
+
+def get_day_of_year(timestamp):
+    time_tuple = time.localtime(timestamp)
+    year_day = time_tuple[7]
+    return year_day
+
 
 def set_rtc_clock():
     try:
         LED_PIN.value(1)
         connect_to_wlan()
         ntptime.settime()	
-        print(f"Time is: ${time.localtime(time.time() + UTC_OFFSET * HOUR)}")
         disconnect_from_wlan()
         LED_PIN.value(0)
     except KeyboardInterrupt:
         machine.reset()
 
-set_rtc_clock()
 
+set_rtc_clock()
 while True:
     current_timestamp = time.time() + UTC_OFFSET * HOUR
-    if is_watering_time(current_timestamp):
+    if is_watering_time(current_timestamp) and is_watering_day(current_timestamp):
         open_pump()
 
     time_to_sleep = get_time_to_sleep(current_timestamp)
-    print(f"Sleeping: {time_to_sleep}")
+    print(time_to_sleep)
     sleep(time_to_sleep)
