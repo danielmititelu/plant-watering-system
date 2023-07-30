ebe79c94f7829194f2ca06c8bee923ab355bf144
Daniel Mititelu <mttl.daniel@gmail.com>
2023-07-30 09:40:21 +0300

    Add Collector

---
+++ Collector/Collector/Controllers/VoltageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Collector.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VoltageController : ControllerBase
+    {
+        private readonly ILogger<VoltageController> _logger;
+        public VoltageController(ILogger<VoltageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] float voltage)
+        {
+            _logger.LogInformation("{voltage}", voltage);
+            return Ok();
+        }
+    }
+}
